054a09c7aed78c4d8ce78be13780522e8f25f98f
davygora <glory-gerrard@ukr.net>
2016-03-17 19:08:07 +0200

    create some .scss

--- app/models/petition.rb
+++ app/models/petition.rb
@@ -2,6 +2,7 @@ class Petition < ActiveRecord::Base
 
   belongs_to :user
   has_reputation :votes, source: :user, aggregated_by: :sum
-  validates :title, presence: true, uniqueness: true, length: { in: 5..30 }
+  validates :title, presence: true, uniqueness: true, length: { in: 10..200 }
   validates :text, presence: true
+
 end
