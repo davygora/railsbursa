d31b352a909a7d32a2f1dd6644ee50c5ae275e5d
davygora <glory-gerrard@ukr.net>
2016-03-19 21:06:38 +0200

    If the petition is older than 30 days - dont show the Vote button

--- app/models/petition.rb
+++ app/models/petition.rb
@@ -2,6 +2,11 @@ class Petition < ActiveRecord::Base
 
   belongs_to :user
   has_reputation :votes, source: :user, aggregated_by: :sum
+
   validates :title, presence: true, uniqueness: true, length: { in: 5..30 }
   validates :text, presence: true
+
+  def published?
+    created_at < 30.day.ago
+  end
 end
