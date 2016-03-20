7646846f82c9e94d5aec8e09f2a8f06123b45ccd
davygora <glory-gerrard@ukr.net>
2016-03-20 23:43:17 +0200

    Change..

--- config/schedule.rb
+++ config/schedule.rb
@@ -2,7 +2,6 @@ set :enviroment, :development
 
 set :output, "log/cron_log.log"
 
-#every :day, at: "00:01" do
-every 1.minutes do
+every :day, at: "00:01" do
   rake "petitions:check_petition"
 end 
