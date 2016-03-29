d46422a858a45f41a044cacbe58b99ce9f7e62d4
davygora <glory-gerrard@ukr.net>
2016-03-29 20:06:52 +0300

    update home page & navbar

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,5 @@
 class ApplicationController < ActionController::Base
+
   protect_from_forgery with: :exception
 
 private
@@ -6,9 +7,13 @@ private
   def current_user
     @current_user ||= User.find(session[:user_id]) if session[:user_id]
   end
+
   helper_method :current_user
 
   def authorize
-    redirect_to login_url, alert: "Not authorized" if current_user.nil?
+    if current_user.nil?
+      flash[:danger] = "Not authorized"
+      redirect_to login_url
+    end
   end
 end
