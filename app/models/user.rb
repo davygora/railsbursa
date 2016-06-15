d30062acd28fdda2f3ad16d857cdeb6fdbcd85b3
davygora <glory-gerrard@ukr.net>
2016-06-15 20:37:35 +0300

    Change validates

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,10 +2,11 @@ class User < ActiveRecord::Base
 
   has_many :petitions
   has_many :evaluations, class_name: "ReputationSystem::Evaluation", as: :source
+
   has_secure_password
 
   validates :email, presence: true, uniqueness: true
-  validates :password, presence: true, length: { minimum: 5 }
+  validates :password, presence: true, length: { minimum: 6 }
 
   def voted_for?(petition)
     evaluations.where(target_type: petition.class, target_id: petition.id).present?
